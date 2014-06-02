1a641abe18070cbfcd9835e0d03fb7a8b7a003da
Stewart Anderson <stewie1570@gmail.com>
2014-06-02 01:40:46 -0600

    Initial commit...

---
+++ Cache.Tests/CacheTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Cache.Tests
+{
+    [TestClass]
+    public class CacheTest
+    {
+        private Cache _cache;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _cache = new Cache();
+        }
+
+        [TestMethod]
+        public async Task ShouldReturnResultFromDataSourceTask()
+        {
+            //Arrange
+            //Act
+            int result = await _cache.Get("some key", () => Task.FromResult(2));
+
+            //Assert
+            result.Should().Be(2);
+        }
+
+        [TestMethod]
+        public async Task ShouldAwaitDataSourceTaskOnceAndReturnTheFirstDataSourceResult()
+        {
+            //Arrange
+            int callCount = 0;
+
+            //Act
+            await _cache.Get("some key", () => { callCount++; return Task.FromResult(2); });
+            var result = await _cache.Get("some key", () => { callCount++; return Task.FromResult(3); });
+
+            //Assert
+            callCount.Should().Be(1);
+            result.Should().Be(2);
+        }
+
+        [TestMethod]
+        public async Task ShouldAwaitDataSourceTaskTwiceSinceKeyExpired()
+        {
+            //Arrange
+            var time = DateTime.Parse("01/01/2000 12:00 am");
+            _cache = new Cache(() => time, TimeSpan.FromMinutes(1));
+            int callCount = 0;
+            Func<Task<int>> func = () => { callCount++; return Task.FromResult(2); };
+
+            //Act
+            await _cache.Get("some key", func);
+            time = DateTime.Parse("01/01/2000 12:01 am");
+            await _cache.Get("some key", func);
+
+            //Assert
+            callCount.Should().Be(2);
+        }
+    }
+}
---
+++ Cache/Cache.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cache
+{
+    public class Cache
+    {
+        private Dictionary<string, CacheItem> _dictionary = new Dictionary<string, CacheItem>();
+        private Func<DateTime> _timeProvider;
+        private TimeSpan _keyLifeTime;
+
+        public Cache() : this(() => DateTime.UtcNow, TimeSpan.FromMinutes(5)) { }
+        public Cache(Func<DateTime> timeProvider, TimeSpan keyLifeTime)
+        {
+            _timeProvider = timeProvider;
+            _keyLifeTime = keyLifeTime;
+        }
+
+        //TODO: This is not thread-safe yet!!!
+        public async Task<T> Get<T>(string key, Func<Task<T>> cacheDataSource)
+        {
+            var currentTime = _timeProvider();
+
+            if (!_dictionary.ContainsKey(key) || currentTime >= _dictionary[key].Expiration)
+            {
+                _dictionary[key] = new CacheItem
+                {
+                    Item = await cacheDataSource(),
+                    Expiration = currentTime + _keyLifeTime
+                };
+            }
+            return (T)_dictionary[key].Item;
+        }
+
+        #region Helper classes
+
+        public class CacheItem
+        {
+            public object Item { get; set; }
+            public DateTime Expiration { get; set; }
+        }
+
+        #endregion
+    }
+}
